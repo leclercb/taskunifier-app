a72075cfb032e9e40ddd33c97631108b4fd2f060
Benjamin Leclerc <benjamin.leclerc@me.com>
2019-04-12 21:09:28 +0200

    Configure electron builder

--- public/electron.js
+++ public/electron.js
@@ -1,31 +1,44 @@
 const { app, ipcMain, BrowserWindow } = require('electron');
 const path = require('path');
-const url = require('url');
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const isDevelopment = require('electron-is-dev');
 
 let mainWindow = null;
 
 function createMainWindow() {
     const window = new BrowserWindow({
         show: false,
+        titleBarStyle: 'hidden',
+        webPreferences: {
+            nodeIntegration: false
+        },
         width: 1024,
         height: 768,
         icon: 'public/resources/images/logo.png'
     });
 
     if (isDevelopment) {
+        const {
+            default: installExtension,
+            REACT_DEVELOPER_TOOLS,
+            REDUX_DEVTOOLS,
+        } = require('electron-devtools-installer');
+
+        installExtension(REACT_DEVELOPER_TOOLS)
+            .then(name => { console.log(`Added Extension: ${name}`); })
+            .catch(err => { console.log('An error occurred: ', err); });
+
+        installExtension(REDUX_DEVTOOLS)
+            .then(name => { console.log(`Added Extension: ${name}`); })
+            .catch(err => { console.log('An error occurred: ', err); });
+
         window.webContents.openDevTools();
     }
 
     if (isDevelopment) {
         window.loadURL('http://localhost:3000');
     } else {
-        window.loadURL(url.format({
-            pathname: path.join(__dirname, 'index.html'),
-            protocol: 'file',
-            slashes: true
-        }));
+        window.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
     }
 
     window.once('ready-to-show', () => {
