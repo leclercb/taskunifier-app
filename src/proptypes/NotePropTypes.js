d81c772edcc27e9e4dca77929760d8a0bfa62981
Benjamin Leclerc <benjamin.leclerc@me.com>
2019-08-03 16:20:41 +0200

    Add color to note prop types

--- src/proptypes/NotePropTypes.js
+++ src/proptypes/NotePropTypes.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 export const NotePropType = PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    color: PropTypes.string,
     star: PropTypes.bool,
     tags: PropTypes.arrayOf(PropTypes.string.isRequired),
     folder: PropTypes.string
