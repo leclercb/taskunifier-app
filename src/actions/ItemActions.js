6a94c794f2345f374429c94a8ad69f520ec76cd5
Benjamin Leclerc <benjamin.leclerc.ext@eurocontrol.int>
2019-08-30 09:43:49 +0000

    Fix send request call in item actions

--- src/actions/ItemActions.js
+++ src/actions/ItemActions.js
@@ -14,7 +14,7 @@ export function buyItem(itemSku, user, email) {
         }));
 
         try {
-            const result = await sendRequest(false, {
+            const result = await sendRequest({
                 method: 'POST',
                 url: `${getConfig().apiUrl}/v1/paypal/orders`,
                 responseType: 'json',
