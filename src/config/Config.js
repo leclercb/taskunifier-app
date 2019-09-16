46f6bcf3112577ac330fd9ffd5767b78bdc7f88f
Benjamin Leclerc <benjamin.leclerc.ext@eurocontrol.int>
2019-09-16 12:42:55 +0000

    Change license keys

--- src/config/Config.js
+++ src/config/Config.js
@@ -21,7 +21,7 @@ const config = {
             }
         },
         license: {
-            publicKey: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArvN3lkMNEgwaTS6yz6y1\n6coi6Jfakg01rYNeOb6x2kv21OEm0Mhq5Q5/Lw+Os2kD30j50IYGvrAB0L7U3kYW\nItVjPnsWqfz81WqzfGRpmLWjnCTdoKyS8uph3F0jaSsrhi0+VsLgFz8p61G5USP7\nE5FDiVgCih5gPPfUTVNk6uIMryLiQpVKsaHvVr/bC7DX0UuKT2PCut88mkARxn51\nCTrEr5aHXxv3e2PwOmdzDTaThjmjjhpwkR8LKuFb71B4e+cJukYOoyJsTa2xqiqf\nzpOv9IGF0By1PJySxY+KZtq/elnhDcct2S28sIh5+3l2fEDz6e8DFLNnHdJ9ofWT\nGQIDAQAB\n-----END PUBLIC KEY-----'
+            publicKey: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4tP95NhDLLMaO7qNfWU5\nHGrnJoVd7MxS0Hc4svmyv4GPa9FBVZPtiQwVOm4OpkFLBxIxr/c3lxIecar7a53R\nepcoTLs1qaZeuBwTruOiHHbkbwA8u/ntrJIahtqMLZ9HH8yLJaymC2OLjaT18hcZ\n8W7n89fSR262ZZcd0joY4eTpF/sEnCZpccpBGKEwVHtOFyna+E+XzgQrSFe+0qEz\nPIU9P8gGDmuBm8xXx6w8OwKSM3jAQ01ja7l5epsrNWXD8z3PJrOom5cigiu8l1Sk\nJJoWoH5gNUNOqMqeH4mpCbfn6YF69aNAVrZcXkB+rkGFdgdQrQXWyyJufFKviSyd\niwIDAQAB\n-----END PUBLIC KEY-----'
         },
         synchronization: {
             taskunifier: {
@@ -52,7 +52,7 @@ const config = {
             }
         },
         license: {
-            publicKey: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArvN3lkMNEgwaTS6yz6y1\n6coi6Jfakg01rYNeOb6x2kv21OEm0Mhq5Q5/Lw+Os2kD30j50IYGvrAB0L7U3kYW\nItVjPnsWqfz81WqzfGRpmLWjnCTdoKyS8uph3F0jaSsrhi0+VsLgFz8p61G5USP7\nE5FDiVgCih5gPPfUTVNk6uIMryLiQpVKsaHvVr/bC7DX0UuKT2PCut88mkARxn51\nCTrEr5aHXxv3e2PwOmdzDTaThjmjjhpwkR8LKuFb71B4e+cJukYOoyJsTa2xqiqf\nzpOv9IGF0By1PJySxY+KZtq/elnhDcct2S28sIh5+3l2fEDz6e8DFLNnHdJ9ofWT\nGQIDAQAB\n-----END PUBLIC KEY-----'
+            publicKey: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4tP95NhDLLMaO7qNfWU5\nHGrnJoVd7MxS0Hc4svmyv4GPa9FBVZPtiQwVOm4OpkFLBxIxr/c3lxIecar7a53R\nepcoTLs1qaZeuBwTruOiHHbkbwA8u/ntrJIahtqMLZ9HH8yLJaymC2OLjaT18hcZ\n8W7n89fSR262ZZcd0joY4eTpF/sEnCZpccpBGKEwVHtOFyna+E+XzgQrSFe+0qEz\nPIU9P8gGDmuBm8xXx6w8OwKSM3jAQ01ja7l5epsrNWXD8z3PJrOom5cigiu8l1Sk\nJJoWoH5gNUNOqMqeH4mpCbfn6YF69aNAVrZcXkB+rkGFdgdQrQXWyyJufFKviSyd\niwIDAQAB\n-----END PUBLIC KEY-----'
         },
         synchronization: {
             taskunifier: {
@@ -83,7 +83,7 @@ const config = {
             }
         },
         license: {
-            publicKey: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArvN3lkMNEgwaTS6yz6y1\n6coi6Jfakg01rYNeOb6x2kv21OEm0Mhq5Q5/Lw+Os2kD30j50IYGvrAB0L7U3kYW\nItVjPnsWqfz81WqzfGRpmLWjnCTdoKyS8uph3F0jaSsrhi0+VsLgFz8p61G5USP7\nE5FDiVgCih5gPPfUTVNk6uIMryLiQpVKsaHvVr/bC7DX0UuKT2PCut88mkARxn51\nCTrEr5aHXxv3e2PwOmdzDTaThjmjjhpwkR8LKuFb71B4e+cJukYOoyJsTa2xqiqf\nzpOv9IGF0By1PJySxY+KZtq/elnhDcct2S28sIh5+3l2fEDz6e8DFLNnHdJ9ofWT\nGQIDAQAB\n-----END PUBLIC KEY-----'
+            publicKey: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAocK4w+shPGvQsDaR8NTn\nRlQy/u7VpzPWVtjTOE+BXNGtlP+NqTmR5kTygr5p83ntcGloL1Qdy7OtF1wtwlBN\n8c9HMHgQcSM8YUkiziCirU2cHy237xtPnIiHN5ygSqsS+8Pyvy7Uhmb/6HXZvUCi\nQCTZsfcm5JxOcbbSLf+rxLIcQRkiSmhLUWoockeN/valHJnd93nHlKyjWKAInY2S\n1YOeXpwKIW8a+Uy3buc1IxnJIM426wk9X4EcH6UL1+0P/vsNG8HkHJNjMRJKoadx\nD9S/Br/isbgm2aVGkNPVq3p2VwxiTFlui1pfRXcgLAKCxv5xvcTL805m8sbWH3K0\nzQIDAQAB\n-----END PUBLIC KEY-----'
         },
         synchronization: {
             taskunifier: {
