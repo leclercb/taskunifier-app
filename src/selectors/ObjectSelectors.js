cddee57a61849b4f6fb60c799c77fe1c3ba1cc5f
Benjamin Leclerc <benjamin.leclerc.ext@eurocontrol.int>
2020-02-04 08:35:33 +0000

    Fix object selector

--- src/selectors/ObjectSelectors.js
+++ src/selectors/ObjectSelectors.js
@@ -1,9 +1,9 @@
 export const getObjects = (state, property) => {
-    if (typeof state[property] === 'object') {
-        return state[property].present;
+    if (Array.isArray(state[property])) {
+        return state[property];
     }
 
-    return state[property];
+    return state[property].present;
 };
 
 export const getObjectById = (state, property, id) => getObjects(state, property).find(object => object.id === id);
