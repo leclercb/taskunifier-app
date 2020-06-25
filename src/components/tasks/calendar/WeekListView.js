b6eb30fbbe359bc44c9d9be49d83c9a53042456e
Benjamin Leclerc <benjamin.leclerc@me.com>
2020-06-25 21:14:07 +0200

    Add drop and drop in week list view

--- src/components/tasks/calendar/WeekListView.js
+++ src/components/tasks/calendar/WeekListView.js
@@ -2,37 +2,27 @@ import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { navigate } from 'react-big-calendar/lib/utils/constants';
+import { useDrag, useDrop } from 'react-dnd';
+import { useTaskApi } from 'hooks/UseTaskApi';
 import 'components/tasks/calendar/WeekListView.css';
 
-function WeekListView({ accessors, components, date, events, localizer }) {
-    const range = WeekListView.range(date);
-    const { event: Event } = components;
+function WeekListView(props) {
+    const taskApi = useTaskApi();
+
+    const range = WeekListView.range(props.date);
 
     return (
         <table className="weekList">
             <thead>
                 <tr>
-                    {range.map(d => (
-                        <th key={d.getTime()}>{localizer.format(d, 'agendaDateFormat')}</th>
+                    {range.map(date => (
+                        <th key={date.getTime()}>{props.localizer.format(date, 'agendaDateFormat')}</th>
                     ))}
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    {range.map(d => (
-                        <td key={d.getTime()}>
-                            {events
-                                .filter(event => moment(accessors.start(event)).isSame(moment(d), 'day'))
-                                .map(event => {
-                                    const id = accessors.id(event);
-                                    const title = accessors.title(event);
-
-                                    return (
-                                        <div key={id}>{Event ? <Event event={event} title={title} /> : title}</div>
-                                    );
-                                })}
-                        </td>
-                    ))}
+                    {range.map(date => (<WeekListColumn key={date.getTime()} {...props} date={date} taskApi={taskApi} />))}
                 </tr>
             </tbody>
         </table>
@@ -69,6 +59,73 @@ WeekListView.title = (date, { localizer }) => {
     const start = moment(date).startOf('week').toDate();
     const end = moment(date).endOf('week').toDate();
     return localizer.format({ start, end }, 'agendaHeaderFormat');
+};
+
+function WeekListColumn(props) {
+    const { accessors, date, events, taskApi } = props;
+
+    const [collectedDropProps, drop] = useDrop({
+        accept: 'calendar-event',
+        drop: item => {
+            const dropDate = moment(date);
+            const type = item.data.type;
+            const task = item.data.task;
+
+            if (type === 'startDate' || type === 'dueDate') {
+                const newDate = moment(task[type]);
+                newDate.set('day', dropDate.get('day'));
+                newDate.set('month', dropDate.get('month'));
+                newDate.set('year', dropDate.get('year'));
+
+                taskApi.updateTask({
+                    ...task,
+                    [type]: newDate.toISOString()
+                });
+            }
+        },
+        collect: monitor => ({
+            hovered: monitor.isOver()
+        })
+    });
+
+    return (
+        <td ref={drop} className={collectedDropProps.hovered ? 'hovered' : null}>
+            {events
+                .filter(event => moment(accessors.start(event)).isSame(moment(date), 'day'))
+                .map(event => (<WeekListCell key={event.id} {...props} event={event} />))}
+        </td>
+    );
+}
+
+WeekListColumn.propTypes = {
+    accessors: PropTypes.object.isRequired,
+    date: PropTypes.instanceOf(Date),
+    events: PropTypes.array,
+    taskApi: PropTypes.object
+};
+
+function WeekListCell({ accessors, components, event }) {
+    const { event: Event } = components;
+
+    const title = accessors.title(event);
+
+    // eslint-disable-next-line no-unused-vars
+    const [collectedDragProps, drag] = useDrag({
+        item: {
+            type: 'calendar-event',
+            data: event
+        }
+    });
+
+    return (
+        <div ref={drag} style={{ cursor: 'pointer' }}>{Event ? <Event event={event} title={title} /> : title}</div>
+    );
+}
+
+WeekListCell.propTypes = {
+    accessors: PropTypes.object.isRequired,
+    components: PropTypes.object.isRequired,
+    event: PropTypes.object
 };
 
 export default WeekListView;
